c0e261c9545b4961a05da7a3029cbbaadae97cac
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-12-15 17:10:48 -0800

    Adjusted Llama spit to spawn on head of player and play sound.

--- src/main/java/com/stanthemudkip/spotions/networking/packet/ExampleC2SPacket.java
+++ src/main/java/com/stanthemudkip/spotions/networking/packet/ExampleC2SPacket.java
@@ -3,9 +3,12 @@ package com.stanthemudkip.spotions.networking.packet;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraftforge.network.NetworkEvent;
+
 
 import java.util.function.Supplier;
 
@@ -34,9 +37,11 @@ public class ExampleC2SPacket {
             ServerLevel level = player.getLevel();
 
             //Spawn some LLama spit on the player :)
-            EntityType.LLAMA_SPIT.spawn(level, null, null, player.blockPosition(),
+            EntityType.LLAMA_SPIT.spawn(level, null, null, player.getOnPos().offset(0, 3, 0),
                     MobSpawnType.COMMAND, true, false);
-
+            //Play the Llama spit noise
+            level.playSound(null, player.getOnPos(), SoundEvents.LLAMA_SPIT, SoundSource.PLAYERS,
+                    0.5f,level.random.nextFloat() * 0.1f + 0.9f);
         });
 
         return true;
