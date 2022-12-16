9ec94a22a1553b0c9cc5c7e4204d56b6b4186750
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-12-15 18:16:30 -0800

    Adjusted Byrill crushing with Mortar and Pestle to spawn the dust item entity right at the player's feet and can always be picked up!!!!!!!!!

--- src/main/java/com/stanthemudkip/spotions/networking/packet/Byrill_Mortar_PestleC2SPacket.java
+++ src/main/java/com/stanthemudkip/spotions/networking/packet/Byrill_Mortar_PestleC2SPacket.java
@@ -1,13 +1,16 @@
 package com.stanthemudkip.spotions.networking.packet;
 
 import com.stanthemudkip.spotions.item.ModItems;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkEvent;
 
@@ -39,7 +42,10 @@ public class Byrill_Mortar_PestleC2SPacket {
 
             //Spawn some Byrill_Dust on the player :)
             ItemStack itemStack1 = new ItemStack(ModItems.BYRILL_DUST.get().getDefaultInstance().getItem(), 1);
-            player.drop(itemStack1, true);
+            ItemEntity itemEntity = new ItemEntity(level, player.getX(), player.getY(), player.getZ(), itemStack1);
+            itemEntity.setPickUpDelay(0);
+            level.addFreshEntity(itemEntity);
+
             level.playSound(null, player.getOnPos(), SoundEvents.LARGE_AMETHYST_BUD_BREAK , SoundSource.PLAYERS,
                     0.5f,level.random.nextFloat() * 0.1f + 0.9f);
         });
